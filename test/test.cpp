0bbf6850de150a874f38e48735a62e8866deb353
Riley Levy <rileylevy95@gmail.com>
2022-05-07 18:22:47 -0400

    Add+use test_that_compile_errors cmake function

--- test/test.cpp
+++ test/test.cpp
@@ -7,10 +7,14 @@
 #include <type_traits>
 
 template<class T>
-constexpr bool is_read_only =
-    std::is_const_v<std::remove_reference_t<T>>;
+constexpr bool is_read_only = std::is_const_v<std::remove_reference_t<T>>;
 
 #define IS_READ_ONLY(x) is_read_only<decltype(x)>
+
+TEST_CASE("Shadow allows shadowing without warnings") {
+  [[maybe_unused]] constexpr  int x = 0;
+  UMBRA_SHADOW(constexpr char x = 'a') { STATIC_REQUIRE(x == 'a'); }
+}
 
 TEST_CASE("Frozen variables are const ref") {
   int  x = 0;
